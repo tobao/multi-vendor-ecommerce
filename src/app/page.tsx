bafa4996e860f0ce318adb64c1cd4c66cf97a1b2
BaoTo <tobao97@gmail.com>
2025-01-20 11:42:33 +0700

    Installing Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className=" text-blue-500">Welcome to the course</h1>
       <h1 className=" text-blue-500 font-barlow">Welcome to the course</h1>
+      <Button variant='destructive'>Click here</Button>
     </div>
   );
 }
