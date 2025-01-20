5ce8b8a4c0645d1bd7155d43b3f6cb41f37209b0
BaoTo <tobao97@gmail.com>
2025-01-20 10:38:55 +0700

    Optimizing Metadata and Selecting the Perfect Fonts for Our Web App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-blue-500">Welcome to the course</h1>
+      <h1 className=" text-blue-500">Welcome to the course</h1>
+      <h1 className=" text-blue-500 font-barlow">Welcome to the course</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        barlow: ["var(--font-barlow)", "sans-serif"],
+      } 
     },
   },
   plugins: [],
